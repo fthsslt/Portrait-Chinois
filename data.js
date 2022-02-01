1019a2d094a64bede7416a96a1d52a7db32cbf1f
fthsslt <fathallah.zobir@edu.univ-eiffel.fr>
2022-02-01 09:51:18 +0100

    Portrait

---
+++ data.js
@@ -0,0 +1,47 @@
+
+
+
+
+
+var analogies = ["Accueil",
+    "Si j'étais un animé je serais" ,
+    "Si j'étais une musique je serais" ,
+    "Si j'étais un rappeur je serais" ,
+  "Si j'étais Jeux vidéo je serais",
+    "Si j'étais un acteur je serais " ,
+    "Si j'étais un logiciel je serais" ,
+    "Si j'étais un album je serais" ,
+];
+
+var quoi = ["Bienvenue",
+"Détective Conan" ,
+"CQFD" ,
+"DAMSO" ,
+"Assassin's Creed III" ,
+"Keanu Reeves " ,
+"FL Studio" ,
+"Dans la légende" ,
+];
+
+var explicationn = [
+"Parce que c'est le 1er animé que j'ai vu. Comme le nom l'indique, c'est un animé d'enquête policière, c'est très captivant. ",
+
+
+"Parce que c'est la musique que j'ai le plus écouté. Cette musique m'a beaucoup accompagné dans les moments révision, c'est une musique très imagée on peut donc facilement imaginer et comprendre le sens derrière les paroles, il y a certaines vérités.",
+
+
+"Parce que j'aime beaucoup sa plume. Il est l'un des artistes que j'ai le plus écouté dans ma vie.C'est un artiste très polyvalent, il a fait ses preuves dans le rap, et même dans ce qu'on appelle la zumba.J'aime beaucoup son côté discret, il n'a pas besoin d'être toujours devant les projecteurs, mais malgré lui, on parle souvent de lui et de son évolution.",
+
+
+" Parce que c'est une série de jeux vidéo qui fait partie des classiques aujourd'hui. J'ai choisi le 3 car l'histoire se passait pendant la révolution américaine, dans le jeu, on rencontre les grandes personnes qui ont participé à cette révolution comme Georges Washington.",
+
+
+"Parce que c'est un acteur qui était là depuis mon plus jeune âge, en étant petit, j'ai regardé Matrix et maintenant que je suis grand, je regarde John Wick . C'est un acteur qui dégage beaucoup de bonne humeur en interview et d'après les personnes qui l'ont rencontré, c'est un humain très humble.",
+
+
+" Parce que c'est un logiciel de production de musique. J'aime la musique, j'ai grandi entouré de gens qui aiment la musique. Fl studio me permets d'être très créatif. C'est un logiciel complet et très facile à prendre en main.",
+
+
+"Parce que pour moi, c'est plus qu'un album, c'est un film, car sur Youtube, on peut retrouver 4 clips qui racontent une histoire.Au début comme beaucoup d'autres personnes j'étais pas attiré par l'album et ce que propose le duo Pnl mais grâce aux clips sur Youtube je me suis plongé dans leur univers . Jamais un album n'a aussi bien porté son nom, cet album restera dans la légende des meilleurs albums que j'ai écouté.",
+]
+
